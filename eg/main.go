02fb76caf7237f6408cb39163f544315a3557ab8
Andrew Rodland <andrew@cleverdomain.org>
2020-03-26 10:46:40 -0400

    Rename client object from cli to fc
    I just like this name better, and it's clearer since the Flex API itself
    has "client" objects

--- eg/main.go
+++ eg/main.go
@@ -11,14 +11,14 @@ import (
 )
 
 func main() {
-	cli, err := flexclient.NewFlexClient(os.Args[1])
+	fc, err := flexclient.NewFlexClient(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
 	go func() {
 		messages := make(chan flexclient.Message)
-		cli.SetMessageChan(messages)
+		fc.SetMessageChan(messages)
 		for msg := range messages {
 			fmt.Printf("M[%s]%s\n", msg.SenderHandle, msg.Message)
 		}
@@ -26,7 +26,7 @@ func main() {
 
 	go func() {
 		updates := make(chan flexclient.StateUpdate)
-		cli.SetStateChan(updates)
+		fc.SetStateChan(updates)
 		for upd := range updates {
 			fmt.Printf("S[%s]%s: %v -> %v\n", upd.SenderHandle, upd.Object, upd.Updated, upd.CurrentState)
 		}
@@ -39,7 +39,7 @@ func main() {
 		}
 
 		vitaPackets := make(chan flexclient.VitaPacket)
-		cli.SetVitaChan(vitaPackets)
+		fc.SetVitaChan(vitaPackets)
 		for pkt := range vitaPackets {
 			if pkt.Preamble.Class_id.PacketClassCode == 0x03e3 {
 				file.Write(pkt.Payload)
@@ -51,28 +51,28 @@ func main() {
 	wg.Add(1)
 
 	go func() {
-		cli.Run()
+		fc.Run()
 		wg.Done()
 	}()
 
-	cli.StartUDP()
-	cli.SendAndWait("sub slice all")
+	fc.StartUDP()
+	fc.SendAndWait("sub slice all")
 
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
 		_ = <-c
 		fmt.Println("Exit on SIGINT")
-		cli.Close()
+		fc.Close()
 	}()
 
 	go func() {
 		lines := bufio.NewScanner(os.Stdin)
 		for lines.Scan() {
-			res := cli.SendAndWait(lines.Text())
+			res := fc.SendAndWait(lines.Text())
 			fmt.Println(res)
 		}
-		cli.Close()
+		fc.Close()
 	}()
 
 	wg.Wait()
