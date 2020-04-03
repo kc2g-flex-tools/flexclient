2c751c2755aee6377adceef0d83f9edb6dbe6b15
Andrew Rodland <andrew@cleverdomain.org>
2020-04-03 00:15:22 -0400

    Add a bunch of setters that send a command and update radio state
    When you subscribe to an object, the radio sends back all changes to an
    object *except* the ones you made (well, sometimes it sends those too,
    but not always). So if you want to maintain accurate state, you need to
    keep track of set commands.

---
+++ setters.go
@@ -0,0 +1,50 @@
+package flexclient
+
+import "fmt"
+
+func (f *FlexClient) sendAndUpdateObj(cmd, object string, changes Object) CmdResult {
+	// TODO: do a dance with "locking" updateState somehow so that we can get the
+	// cmdresult without any intermediate notifications being applied, then apply
+	// our changes, then unlock updateState. That way if anything the radio sends
+	// back contradicts the changes provided, the radio will win.
+	f.Lock()
+	defer f.Unlock()
+	res := f.SendAndWait(cmd)
+
+	if res.Error == 0 {
+		f.updateState("", object, changes)
+	}
+
+	return res
+}
+
+func (f *FlexClient) setAndUpdateObj(cmdPrefix, object string, changes Object) CmdResult {
+	cmd := cmdPrefix
+	for k, v := range changes {
+		// TODO: validate k and v are legal (no spaces or equals)
+		cmd += " " + k + "=" + v
+	}
+
+	return f.sendAndUpdateObj(cmd, object, changes)
+}
+
+func (f *FlexClient) SliceSet(sliceIdx string, values Object) CmdResult {
+	return f.setAndUpdateObj("slice set "+sliceIdx, "slice "+sliceIdx, values)
+}
+
+func (f *FlexClient) SliceTune(sliceIdx string, freq float64) CmdResult {
+	freqStr := fmt.Sprintf("%.6f", freq)
+	return f.sendAndUpdateObj(
+		"slice t "+sliceIdx+" "+freqStr,
+		"slice "+sliceIdx,
+		Object(map[string]string{"RF_frequency": freqStr}),
+	)
+}
+
+func (f *FlexClient) TransmitSet(values Object) CmdResult {
+	return f.setAndUpdateObj("transmit", "transmit", values)
+}
+
+func (f *FlexClient) RadioSet(values Object) CmdResult {
+	return f.setAndUpdateObj("radio set", "radio", values)
+}
