6a5e639f70fb305b25968cda9a12c99dc23e1733
Andrew Rodland <andrew@cleverdomain.org>
2020-06-16 01:14:52 -0400

    Make the eg utility build again, and beautify output

--- eg/main.go
+++ eg/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sort"
 	"sync"
 
-	"github.com/arodland/flexclient"
+	"github.com/kc2g-flex-tools/flexclient"
 )
 
 func main() {
@@ -29,27 +30,51 @@ func main() {
 	}()
 
 	go func() {
+		const (
+			Normal       = "\x1b[0m"
+			Black        = "\x1b[30m"
+			Red          = "\x1b[31m"
+			Green        = "\x1b[32m"
+			Brown        = "\x1b[33m"
+			Blue         = "\x1b[34m"
+			Magenta      = "\x1b[35m"
+			Cyan         = "\x1b[36m"
+			LightGray    = "\x1b[37m"
+			DarkGray     = "\x1b[30;1m"
+			LightRed     = "\x1b[31;1m"
+			LightGreen   = "\x1b[32;1m"
+			LightYellow  = "\x1b[33;1m"
+			LightBlue    = "\x1b[34;1m"
+			LightMagenta = "\x1b[35;1m"
+			LightCyan    = "\x1b[36;1m"
+			White        = "\x1b[37;1m"
+		)
+
 		updates := make(chan flexclient.StateUpdate, 10)
 		sub := fc.Subscribe(flexclient.Subscription{"", updates})
 		for upd := range updates {
-			fmt.Printf("S[%s]%s: %v -> %v\n", upd.SenderHandle, upd.Object, upd.Updated, upd.CurrentState)
+			fmt.Printf("S[%s] %s: ", upd.SenderHandle, upd.Object)
+			keys := []string{}
+			for key := range upd.CurrentState {
+				keys = append(keys, key)
+			}
+			sort.Strings(keys)
+			space := ""
+			for _, key := range keys {
+				var keyColor, valueColor string
+				if _, ok := upd.Updated[key]; ok {
+					keyColor = LightGreen
+					valueColor = LightBlue
+				} else {
+					keyColor = Green
+					valueColor = Blue
+				}
+				fmt.Printf("%s%s%s%s=%s%s%s", space, keyColor, key, Normal, valueColor, upd.CurrentState[key], Normal)
+				space = " "
+			}
+			fmt.Println("")
 		}
 		fc.Unsubscribe(sub)
-	}()
-
-	go func() {
-		file, err := os.Create("out.raw")
-		if err != nil {
-			panic(err)
-		}
-
-		vitaPackets := make(chan flexclient.VitaPacket)
-		fc.SetVitaChan(vitaPackets)
-		for pkt := range vitaPackets {
-			if pkt.Preamble.Class_id.PacketClassCode == 0x03e3 {
-				file.Write(pkt.Payload)
-			}
-		}
 	}()
 
 	var wg sync.WaitGroup
@@ -62,9 +87,6 @@ func main() {
 
 	fc.StartUDP()
 	fc.SendAndWait("sub slice all")
-
-	slices := fc.FindObjects("slice ")
-	fmt.Printf("%#v\n", slices)
 
 	go func() {
 		c := make(chan os.Signal, 1)
