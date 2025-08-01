03f34e65f288984254388c538862deb469a94b59
Andrew Rodland <andrew@cleverdomain.org>
2025-08-01 03:31:58 -0400

    BREAKING CHANGE: all state-aware setters now take a context

--- setters.go
+++ setters.go
@@ -1,16 +1,20 @@
 package flexclient
 
 import (
+	"context"
 	"fmt"
 	"strconv"
 )
 
-func (f *FlexClient) sendAndUpdateObj(cmd, object string, changes Object) CmdResult {
+func (f *FlexClient) sendAndUpdateObj(ctx context.Context, cmd, object string, changes Object) (CmdResult, error) {
 	// TODO: do a dance with "locking" updateState somehow so that we can get the
 	// cmdresult without any intermediate notifications being applied, then apply
 	// our changes, then unlock updateState. That way if anything the radio sends
 	// back contradicts the changes provided, the radio will win.
-	res := f.SendAndWait(cmd)
+	res, err := f.SendAndWaitContext(ctx, cmd)
+	if err != nil {
+		return res, err
+	}
 
 	if res.Error == 0 {
 		f.Lock()
@@ -18,62 +22,69 @@ func (f *FlexClient) sendAndUpdateObj(cmd, object string, changes Object) CmdRes
 		f.updateState("", object, changes)
 	}
 
-	return res
+	return res, nil
 }
 
-func (f *FlexClient) setAndUpdateObj(cmdPrefix, object string, changes Object) CmdResult {
+func (f *FlexClient) setAndUpdateObj(ctx context.Context, cmdPrefix, object string, changes Object) (CmdResult, error) {
 	cmd := cmdPrefix
 	for k, v := range changes {
 		// TODO: validate k and v are legal (no spaces or equals)
 		cmd += " " + k + "=" + v
 	}
 
-	return f.sendAndUpdateObj(cmd, object, changes)
+	return f.sendAndUpdateObj(ctx, cmd, object, changes)
 }
 
-func (f *FlexClient) SliceSet(sliceIdx string, values Object) CmdResult {
-	return f.setAndUpdateObj("slice set "+sliceIdx, "slice "+sliceIdx, values)
+func (f *FlexClient) SliceSet(ctx context.Context, sliceIdx string, values Object) (CmdResult, error) {
+	return f.setAndUpdateObj(ctx, "slice set "+sliceIdx, "slice "+sliceIdx, values)
 }
 
-func (f *FlexClient) SliceTune(sliceIdx string, freq float64) CmdResult {
+func (f *FlexClient) SliceTune(ctx context.Context, sliceIdx string, freq float64) (CmdResult, error) {
 	freqStr := fmt.Sprintf("%.6f", freq)
 	return f.sendAndUpdateObj(
+		ctx,
 		"slice t "+sliceIdx+" "+freqStr,
 		"slice "+sliceIdx,
 		Object{"RF_frequency": freqStr},
 	)
 }
 
-func (f *FlexClient) SliceSetFilter(sliceIdx string, filterLo, filterHi int) CmdResult {
+func (f *FlexClient) SliceSetFilter(ctx context.Context, sliceIdx string, filterLo, filterHi int) (CmdResult, error) {
 	lo := strconv.Itoa(filterLo)
 	hi := strconv.Itoa(filterHi)
 	return f.sendAndUpdateObj(
+		ctx,
 		"filt "+sliceIdx+" "+lo+" "+hi,
 		"slice "+sliceIdx,
 		Object{"filter_lo": lo, "filter_hi": hi},
 	)
 }
 
-func (f *FlexClient) TransmitSet(values Object) CmdResult {
-	return f.setAndUpdateObj("transmit set", "transmit", values)
+func (f *FlexClient) TransmitSet(ctx context.Context, values Object) (CmdResult, error) {
+	return f.setAndUpdateObj(ctx, "transmit set", "transmit", values)
 }
 
-func (f *FlexClient) TransmitTune(val string) CmdResult {
+func (f *FlexClient) TransmitTune(ctx context.Context, val string) (CmdResult, error) {
 	return f.sendAndUpdateObj(
+		ctx,
 		"transmit tune "+val,
 		"transmit",
 		Object{"tune": val},
 	)
 }
 
-func (f *FlexClient) RadioSet(values Object) CmdResult {
-	return f.setAndUpdateObj("radio set", "radio", values)
+func (f *FlexClient) RadioSet(ctx context.Context, values Object) (CmdResult, error) {
+	return f.setAndUpdateObj(ctx, "radio set", "radio", values)
 }
 
-func (f *FlexClient) PanSet(id string, values Object) CmdResult {
-	result := f.setAndUpdateObj("display pan set "+id, "display pan "+id, values)
+func (f *FlexClient) PanSet(ctx context.Context, id string, values Object) (CmdResult, error) {
+	result, err := f.setAndUpdateObj(ctx, "display pan set "+id, "display pan "+id, values)
+	if err != nil {
+		return result, err
+	}
+
 	if result.Error != 0 {
-		return result
+		return result, nil
 	}
 
 	// Funny API quirk: updating a panadapter also updates an associated waterfall
@@ -83,15 +94,15 @@ func (f *FlexClient) PanSet(id string, values Object) CmdResult {
 	defer f.Unlock()
 	pan, ok := f.getObject("display pan " + id)
 	if !ok {
-		return result
+		return result, fmt.Errorf("pan %s not found", id)
 	}
 	childWaterfallId, ok := pan["waterfall"]
 	if !ok {
-		return result
+		return result, fmt.Errorf("pan %s has no associated waterfall", id)
 	}
 	wf, ok := f.getObject("display waterfall " + childWaterfallId)
 	if !ok {
-		return result
+		return result, fmt.Errorf("waterfall %s not found", childWaterfallId)
 	}
 	var setWf = values.Copy()
 	for key := range values {
@@ -102,5 +113,5 @@ func (f *FlexClient) PanSet(id string, values Object) CmdResult {
 	if len(setWf) > 0 {
 		f.updateState("", "display waterfall "+childWaterfallId, setWf)
 	}
-	return result
+	return result, nil
 }
